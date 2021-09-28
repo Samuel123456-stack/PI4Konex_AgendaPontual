dd54e576b3fc7af440e771055c220e517607c90a
Alexsandro Ramos <alexsandrosr@msn.com>
2021-09-28 08:04:55 -0300

    Realizado ajustes no script

--- banco de dados/Script.sql
+++ banco de dados/Script.sql
@@ -8,26 +8,25 @@ USE agendapontual;
 
 CREATE TABLE adm (
     id_adm Integer AUTO_INCREMENT PRIMARY KEY,
-    nome varchar(30) not null,
-    email varchar(30) not null,
-    senha varchar(8) not null
+    nome varchar(30) null,
+    email varchar(30) null,
+    senha varchar(8) null
 );
 
 CREATE TABLE endereco (
     id_end Integer AUTO_INCREMENT PRIMARY KEY,
-    logradouro varchar(50) not null,
-    numero Integer not null,
+    logradouro varchar(50) null,
+    numero Integer  null,
     complemento varchar(30) null,
-    bairro varchar(30) not null,
-    estado varchar(30) not null,
-    cep varchar(30) not null
+    bairro varchar(30)  null,
+    estado varchar(30) null,
+    cep varchar(30) null
 );
 
 CREATE TABLE feedback (
     id_feed Integer AUTO_INCREMENT PRIMARY KEY,
     avaliacao Integer null,
     comentario Integer null,
-    identificacao Boolean not null,
     para_medico Boolean null,
     para_consultorio Boolean null,
     anonimo Boolean null
@@ -35,24 +34,24 @@ CREATE TABLE feedback (
 
 CREATE TABLE receita (
     id_rece Integer AUTO_INCREMENT PRIMARY KEY,
-    dataeHora Date not null,
-    informacoes varchar(50) not null
+    dataeHora Date null,
+    informacoes varchar(50) null
 );
 
 CREATE TABLE pagamento (
     id_pag Integer AUTO_INCREMENT PRIMARY KEY,
-    data_pgto Date not null,
-    forma_pagamento varchar(30) not null
+    data_pgto Date null,
+    forma_pagamento varchar(30) null
 );
 
 CREATE TABLE plano (
     id_plan Integer AUTO_INCREMENT PRIMARY KEY,
-    nome varchar(30) not null,
-    valor Double not null,
-    qte_recepcionista integer not null,
-    qte_medico Integer not null,
-    detalhes varchar(50) not null,
-    tolerancia Integer not null,
+    nome varchar(30) null,
+    valor Double null,
+    qte_recepcionista integer null,
+    qte_medico Integer null,
+    detalhes varchar(50) null,
+    tolerancia Integer null,
     fk_pagamento Integer null,
     FOREIGN KEY (fk_pagamento) REFERENCES pagamento (id_pag)
 );
@@ -60,13 +59,13 @@ CREATE TABLE plano (
 
 CREATE TABLE clinica (
     id_cli Integer AUTO_INCREMENT PRIMARY KEY,
-    nome varchar(30) not null,
+    nome varchar(30) null,
     cnpj varchar(18) unique not null,
     status_assina varchar(7) null,
-    qte_assentos Integer not null,
+    qte_assentos Integer null,
     licenca varchar(30) null,
-    email varchar(30) not null,
-    senha varchar(8) not null,
+    email varchar(30) null,
+    senha varchar(8) null,
     fk_plano Integer null,
     fk_endereco Integer null,
     FOREIGN KEY (fk_plano) REFERENCES plano (id_plan),
@@ -75,17 +74,17 @@ CREATE TABLE clinica (
 
 CREATE TABLE medico (
     id_med Integer AUTO_INCREMENT PRIMARY KEY,
-    nome varchar(30) not null,
-    data_nasci Date not null,
-    sexo varchar(1) not null,
-    cpf varchar(11) unique not null,
-    email varchar(30) not null,
-    senha varchar(8) not null,
-    especialidade varchar(30) not null,
-    crm varchar(9) unique not null,
-    fone varchar(11) not null,
-    valor decimal(5,2) not null,
-    data_formatura Date not null,
+    nome varchar(30) null,
+    data_nasci Date null,
+    sexo varchar(1) null,
+    cpf varchar(11) null,
+    email varchar(30) null,
+    senha varchar(8) null,
+    especialidade varchar(30) null,
+    crm varchar(9) null,
+    fone varchar(11) null,
+    valor decimal(5,2) null,
+    data_formatura Date null,
     sobre_mim varchar(50) not null,
     fk_clinica Integer null,
     fk_endereco Integer null,
@@ -95,40 +94,40 @@ CREATE TABLE medico (
 
 CREATE TABLE convenio (
     id_conv Integer AUTO_INCREMENT PRIMARY KEY,
-    nome varchar(30) not null,
-    valorconvenio decimal(5,2) not null
+    nome varchar(30) null,
+    valorconvenio decimal(5,2) null
 );
 
 CREATE TABLE convenio_medico (
-    fk_medico Integer not null,
-    fk_convenio Integer not null,
+    fk_medico Integer null,
+    fk_convenio Integer null,
     FOREIGN KEY (fk_medico) REFERENCES medico (id_med),
     FOREIGN KEY (fk_convenio) REFERENCES convenio (id_conv)
 );
 
 CREATE TABLE triagem (
     id_tria Integer AUTO_INCREMENT PRIMARY KEY,
-    primeira_consulta boolean not null,
+    primeira_consulta boolean null,
     comorbidade Integer null
 );
 
 CREATE TABLE Paciente (
     id_paci Integer AUTO_INCREMENT PRIMARY KEY,
-    nome Varchar(50) not null,
-    cpf varchar(16) not null,
-    telefone varchar(15) not null,
-    convenio varchar(20) not null,
-    email varchar(30) not null,
-    senha varchar(8) not null,
+    nome Varchar(50) null,
+    cpf varchar(16) null,
+    telefone varchar(15) null,
+    convenio varchar(20) null,
+    email varchar(30) null,
+    senha varchar(8) null,
     fk_triagem Integer null,
-    fk_endereco Integer not null,
+    fk_endereco Integer null,
     FOREIGN KEY (fk_triagem) REFERENCES triagem (id_tria),
     FOREIGN KEY (fk_endereco) REFERENCES endereco (id_end)
 );
 
 CREATE TABLE dias_semana (
     id_dia integer AUTO_INCREMENT PRIMARY KEY,
-    dia varchar(13) not null
+    dia varchar(13) null
 );
 
 insert into dias_semana (dia) values
@@ -178,49 +177,45 @@ CREATE TABLE agenda_medica(
 
 CREATE TABLE recepcionista (
     id_recep Integer AUTO_INCREMENT PRIMARY KEY,
-    nome varchar(30) not null,
-    sexo varchar(1) not null,
-    data_nascimento Date not null,
-    cpf varchar(11) not null,
-    email varchar(30) not null,
-    senha varchar(8) not null,
-    fk_clinica Integer not null,
-    fk_endereco Integer not null,
+    nome varchar(30) null,
+    sexo varchar(1)null,
+    data_nascimento Date null,
+    cpf varchar(11) null,
+    email varchar(30) null,
+    senha varchar(8) null,
+    fk_clinica Integer null,
+    fk_endereco Integer null,
     FOREIGN KEY (fk_clinica) REFERENCES clinica (id_cli),
     FOREIGN KEY (fk_endereco) REFERENCES endereco (id_end)
 );
 
 CREATE TABLE notificacao (
     id_notif Integer AUTO_INCREMENT PRIMARY KEY,
-    data_notificacao Date not null,
-    assunto varchar(20) not null,
-    motivo varchar(50) not null
+    data_notificacao Date null,
+    assunto varchar(20) null,
+    motivo varchar(50) null
 );
 
 CREATE TABLE consulta (
 	id_con Integer AUTO_INCREMENT PRIMARY KEY,
-	data_consulta Date not null,
-	hora Time not null,
+	data_consulta Date null,
+	hora Time null,
 	confirmada Boolean null,
 	retorno Boolean null,
 	hora_chegada Time null,
 	hora_saida Time null,
 	duracao Time null,
 	concluida Boolean null,
-	fk_medico Integer not null,
-    fk_paciente Integer not null,
+	fk_medico Integer null,
+    fk_paciente Integer null,
     fk_receita Integer null,
     fk_feedback Integer null,
     fk_notificacao Integer null,
     fk_pagamento Integer null,
-    fk_feedback Integer null,
     FOREIGN KEY (fk_medico) REFERENCES medico (id_med),
     FOREIGN KEY (fk_paciente) REFERENCES paciente (id_paci),
     FOREIGN KEY (fk_receita) REFERENCES receita (id_rece),
-    FOREIGN KEY (fk_feedback) REFERENCES feedback (id_feed),
     FOREIGN KEY (fk_notificacao) REFERENCES notificacao (id_notif),
     FOREIGN KEY (fk_pagamento) REFERENCES pagamento (id_pag),
     FOREIGN KEY (fk_feedback) REFERENCES feedback (id_feed)
 );
-
-

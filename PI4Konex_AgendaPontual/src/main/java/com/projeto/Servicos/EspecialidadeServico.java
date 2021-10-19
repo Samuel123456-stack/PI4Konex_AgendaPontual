ed9dff7075b7eb91080bdb88aca2ca795de1e0fb
Alexsandro Ramos <alexsandrosr@msn.com>
2021-10-19 00:57:26 -0300

    Implementado busca por id de especialidades.

--- PI4Konex_AgendaPontual/src/main/java/com/projeto/Servicos/EspecialidadeServico.java
+++ PI4Konex_AgendaPontual/src/main/java/com/projeto/Servicos/EspecialidadeServico.java
@@ -1,9 +1,11 @@
 package com.projeto.Servicos;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projeto.Entidades.Especialidade;
 import com.projeto.Repositorios.EspecialidadeRepositorio;
@@ -14,7 +16,15 @@ public class EspecialidadeServico {
 	@Autowired
 	private EspecialidadeRepositorio repositorio;
 	
+	@Transactional(readOnly = true)
 	public List<Especialidade> findAll(){
 		return repositorio.findAll();
+	}
+	
+	@Transactional(readOnly = true)
+	public Especialidade findById(Integer id) {
+		Optional<Especialidade> obj = repositorio.findById(id);
+		Especialidade entity = obj.get();
+		return entity;
 	}
 }

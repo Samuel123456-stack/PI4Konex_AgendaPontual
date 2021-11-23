010bc37e81f8e2d4ad9d178a30d3dbd9fa6915ea
Gabriel Felipe <gabrielfelipe_ssantos@hotmail.com>
2021-11-22 23:13:49 -0300

    Implementação da Dash e vinculação ao Th:href nos botões

--- PI4Konex_AgendaPontual/src/main/java/com/projeto/Controladores/DashboardClinicaController.java
+++ PI4Konex_AgendaPontual/src/main/java/com/projeto/Controladores/DashboardClinicaController.java
@@ -1,8 +1,5 @@
 package com.projeto.Controladores;
 
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,55 +10,58 @@ import org.springframework.web.servlet.ModelAndView;
 import com.projeto.Entidades.Agenda;
 import com.projeto.Entidades.Ajuda;
 import com.projeto.Entidades.Feedback;
+import com.projeto.Entidades.Medico;
 import com.projeto.Entidades.Pagamento;
 import com.projeto.Entidades.Recepcionista;
 
 @Controller
 public class DashboardClinicaController {
 	
+	@GetMapping("/viewDash")
+	public ModelAndView viewCli() {
+		ModelAndView mv = new ModelAndView("/clinica/DashboardClinica");
+		return mv;
+	}
+	
 	@GetMapping("/NovoMed")
-	public ModelAndView createMedico(Recepcionista recep){
-		ModelAndView mv = new ModelAndView("/Clinica/CadastroMed");
+	public ModelAndView createMedico(Medico medico){
+		ModelAndView mv = new ModelAndView("/clinica/CadastroMed");
 		return mv;
 	}
 	
 	@GetMapping("/NovoRecep")
 	public ModelAndView createRecep(Recepcionista recep){
-		ModelAndView mv = new ModelAndView("/Clinica/CadastroRecep");
+		ModelAndView mv = new ModelAndView("/clinica/CadastroRecep");
 		return mv;
 	}
 
 	@GetMapping("/ConsultaPagamentos")
 	public ModelAndView listPag(Pagamento pagamentos){
-		ModelAndView mv = new ModelAndView("/Clinica/ConsultarPagamentos");
+		ModelAndView mv = new ModelAndView("/clinica/ConsultarPagamentos");
 		return mv;
 	}
 
 	@GetMapping("/HistoricoAgenda")
 	public ModelAndView listaHistorico(Agenda agenda){
-		ModelAndView mv = new ModelAndView("/Clinica/ConsultarAgendamentos");
+		ModelAndView mv = new ModelAndView("/clinica/ConsultarAgendamentos");
 		return mv;
 	}
 
 	@GetMapping("/FeedbackClinica")
 	public ModelAndView listaFeedbacks(Feedback feedbacks){
-		ModelAndView mv = new ModelAndView("/Clinica/ConsultarFeedback");
+		ModelAndView mv = new ModelAndView("/clinica/ConsultarFeedback");
 		return mv;
 	}
 
 	@GetMapping("/AjudaClinica")
 	public ModelAndView listaFeedbacks(Ajuda ajuda){
-		ModelAndView mv = new ModelAndView("/Clinica/Ajuda");
+		ModelAndView mv = new ModelAndView("/clinica/Ajuda");
 		return mv;
 	}
 	/*
 	@PostMapping("/createAjuda")
 	public ModelAndView createAjuda(@ModelAttribute Ajuda ajuda){
 		ModelAndView mv = new ModelAndView("");
-		Date dataAtual = new Date(0);
-		DateFormat format = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
-		String dataSoli= format.format("dataAtual");
-		ajuda.set
 		return mv;
 	}*/
 
